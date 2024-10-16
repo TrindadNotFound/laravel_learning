2b95a770b7fc6b3a096df1adb5bb8c30ef5334e6
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2024-10-16 22:35:01 +0100

    Continue learning

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
 Route::get('/', [Controllers\IndexController::class, 'Index']);
-Route::get('/about', [Controllers\Aboutcontroller::class, 'About']);
+Route::get('/about', [Controllers\AboutController::class, 'About']);
 Route::get('/contact', [Controllers\ContactController::class, 'Contact']);
 
-Route::get('/contact/{id}', function($id){
-    echo "$id está aqui!";
+Route::get('/contact/{name}/{message?}', function(string $name, string $message="[Value not found]"){
+    echo "Hello $name -- ";
+    echo "Your message value is : $message";
 });

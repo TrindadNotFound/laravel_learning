a6a042fa01751c7307fb173a5645dc05d23bf519
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2024-10-16 22:48:36 +0100

    Continue

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ Route::get('/', [Controllers\IndexController::class, 'Index']);
 Route::get('/about', [Controllers\AboutController::class, 'About']);
 Route::get('/contact', [Controllers\ContactController::class, 'Contact']);
 
-Route::get('/contact/{name}/{message?}', function(string $name, string $message="[Value not found]"){
+//Definicao de uma Route onde 'e feita uma validacao atraves da funcao "where" para saber se os parametros (neste caso job_id e name) cumpem os requisitos :
+// - Ser um inteiro
+// - Ser uma cadeia de caracteres
+Route::get('/contact/{name}/{job_id}', function(string $name='Null', int $job_id=0){
     echo "Hello $name -- ";
-    echo "Your message value is : $message";
-});
+    echo "Your job id is : $job_id";
+})-> where('job_id', '[0-9]+') -> where('name', '[A-Za-z]+');

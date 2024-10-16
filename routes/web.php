929033a08c91efc3c6213d8c50da0b90e0d81749
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2024-10-16 22:25:25 +0100

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [Controllers\IndexController::class, 'Index']);
+Route::get('/about', [Controllers\Aboutcontroller::class, 'About']);
+Route::get('/contact', [Controllers\ContactController::class, 'Contact']);
+
+Route::get('/contact/{id}', function($id){
+    echo "$id está aqui!";
 });
